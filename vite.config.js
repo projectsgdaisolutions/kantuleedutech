8e9c6f5a6d211b924fa78e64790b30258a4a0dcf
thetroviracompany <thetroviracompany@gmail.com>
2026-08-25 15:08:59 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> d1cf86171577974e63affb6285a12aadf71e528a
